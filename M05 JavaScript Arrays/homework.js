ec8d24a343a281ba4af9dfa72775207582c62918
jonnathan <jonsxscar@gmail.com>
2023-02-03 19:10:41 -0500

    fin del 05 y del 06

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -193,12 +193,26 @@ function tablaDelSeis() {
    // Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
    // La función devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
    // Tu código:
+   let seis = [];
+   let resultado = 0;
+   for (let i = 0; i <= 10; i++){
+      resultado = 6 * i;
+      seis.push(resultado);
+   }
+   return seis;
 }
 
 function mayorACien(array) {
    // La función recibe un arreglo con enteros entre 0 y 200.
    // Recorrerlo y retornar un arreglo con todos los valores mayores a 100 (no incluye el 100).
    // Tu código:
+   contador = [];
+   for (let i = 0; i < array.length; i++){
+      if (array[i]>100){
+         contador.push(array[i]);
+      }
+   }
+   return contador;
 }
 
 /* ----------------------------------------------------------------------------------
